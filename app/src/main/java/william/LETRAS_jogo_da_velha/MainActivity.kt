d47a6de9793716032611f65e1aec3f38ed60a4a1
William <william.progdata@gmail.com>
2023-11-07 16:12:32 -0300

    primeiro commit

---
+++ app/src/main/java/william/LETRAS_jogo_da_velha/MainActivity.kt
@@ -0,0 +1,11 @@
+package william.LETRAS_jogo_da_velha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
